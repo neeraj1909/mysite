dbd4bac22e12145f856cf2b6082909ebcf672696
Neeraj Singh <neeraj1909@gmail.com>
2017-04-29 01:39:42 +0530

    views.py

--- polls/views.py
+++ polls/views.py
@@ -27,9 +27,8 @@ def print_square_root(request, number):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
