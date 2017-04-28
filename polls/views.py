3b05952e4db5b9e60aff2d6c4fed7140cfaa2d29
Neeraj Singh <neeraj1909@gmail.com>
2017-04-29 00:40:26 +0530

    views.py

--- polls/views.py
+++ polls/views.py
@@ -4,13 +4,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
 from django.template import loader
+from django.http import Http404
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
-
  
+ 
+ 
+ 
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def print_square_root(request, number):
@@ -21,22 +30,9 @@ def print_square_root(request, number):
 	return HttpResponse(s)
 
 
-
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
-
-
- 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
-
- 
