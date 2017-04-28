29b10caef0cd9fc55da45b1e150dd44b51bf7884
Neeraj Singh <neeraj1909@gmail.com>
2017-04-29 00:17:49 +0530

    views.py

---
+++ polls/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from .models import Question
+from django.template import loader
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+
+def print_square_root(request, number):
+	number = float(number)
+	s = 'Square root of %f is %3.3f.' % (
+		number, number ** 0.5
+	)
+	return HttpResponse(s)
+
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
+ 
+ 
